d34d4b702f9a1b298d914f54b3c8e17ecacedea6
Mike Levy <mike.levy.work@gmail.com>
2013-05-06 20:12:09 +0000

    First steps towards unit testing -- just some infrastructure. This is a very bare-bones Makefile, and the README will be fleshed out as tests get written.
    The goal for this directory is to mimic the "nctest/" directory distributed with the netCDF code, so that's where I'll look for ideas on how to modularize the tests.

---
+++ unittestpio/driver.F90
@@ -0,0 +1,37 @@
+Program pio_unit_test_driver
+
+  Implicit None
+
+  integer, parameter :: str_len = 255
+  character(len=str_len), dimension(3) :: test_file_name = (/"piotest_bin.ieee  ", &
+                                                             "piotest_netcdf.nc ",  &
+                                                             "piotest_pnetcdf.nc"/)
+     
+  integer nfail, ntest
+  logical :: ltest_bin, ltest_netcdf, ltest_pnetcdf
+
+  namelist/piotest_nml/ltest_bin, ltest_netcdf, ltest_pnetcdf
+
+  ltest_bin = .false.
+  ltest_netcdf = .false.
+  ltest_pnetcdf = .false.
+
+  read(*, nml=piotest_nml)
+
+  nfail = 0
+  ntest = 0
+
+  if (ltest_bin) then
+    write(*,"(A)") "Testing PIO's ability to create / read / write a binary file."
+  end if
+
+  if (ltest_netcdf) then
+    write(*,"(A)") "Testing PIO's ability to create / read / write a netcdf file."
+  end if
+
+  if (ltest_netcdf) then
+    write(*,"(A,x,A)") "Testing PIO's ability to create / read / write a", &
+                       "netcdf file using the pnetcdf library."
+  end if
+
+End Program pio_unit_test_driver
---
+++ unittestpio/input.nl
@@ -0,0 +1,5 @@
+&piotest_nml
+ltest_bin     = .true.
+ltest_netcdf  = .true.
+ltest_pnetcdf = .true.
+/
